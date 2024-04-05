917248376e079b3aa1debac2e4743914d1f260f6
plui0002 <plui0002@student.monash.edu>
2024-04-05 15:39:02 +1100

    -add aset -fix register_pokemon -fix get_pokedex_completion

--- poke_team.py
+++ poke_team.py
@@ -2,25 +2,40 @@ from pokemon import *
 import random
 from typing import List
 
+from data_structures.aset import ASet
+from data_structures.referential_array import ArrayR
+
+
 class PokeTeam:
     random.seed(20)
     TEAM_LIMIT = 6
     POKE_LIST = get_all_pokemon_types()
-    pokedex = ArrayR(len(PokeType))
+   
 
 
     def __init__(self):
-        raise NotImplementedError
+        self.team = ArrayR(self.TEAM_LIMIT)
+        
 
     def choose_manually(self):
-        pokeNum = input("Enter the number of your Pokemon")
-        self.team = ArrayR(len(pokeNum))
+        team_count = int(input("Enter the number of your Pokemon: "))
+        self.team = ArrayR(team_count)
+        if team_count > self.TEAM_LIMIT:
+            print("Invaild number")
+            self.choose_manually()
+        else:    
+            count = 0
+            for i in self.POKE_LIST:
+                print(str(count)+": "+i().get_name())
+                count+=1
+        
+            for i in range(team_count):
+                member = int(input("choose your pokemon: "))
+                self.team.__setitem__(i,self.POKE_LIST[member])
+                
 
-        for count in range(0,self.POKE_LIST.__len__()):
-            print(count+1+": \n"+self.POKE_LIST)
-        for i in range(0,pokeNum):
-            choice = input("Choose your "+i+"th Pokemon")
-            self.team.__setitem__(i,self.POKE_LIST[choice+1])
+        
+        
                 
     def choose_randomly(self) -> None:
         all_pokemon = get_all_pokemon_types()
@@ -33,14 +48,19 @@ class PokeTeam:
     def regenerate_team(self) -> None:
         raise NotImplementedError
     
+
+    def assemble_team(self):
+        raise NotImplementedError
+    
+
     def special(self) -> None:
         raise NotImplementedError
 
     def __getitem__(self, index: int):
-        return self.team[index]
+        return self.team.__getitem__(index)
 
     def __len__(self):
-        return self.len(self.team)
+        return self.team.__len__()
 
     def __str__(self):
         return self.team[0:]+"\n"
@@ -48,50 +68,47 @@ class PokeTeam:
 class Trainer:
 
     def __init__(self, name) -> None:
-        self.name = name
+        self.TrainerName = name
+        self.TrainerTeam = PokeTeam()
+        self.pokedex = ASet(15)
         
 
     def pick_team(self, method: str) -> None:
-        userinput = input("[R]Random team\n[M]Manual team")
+        userinput = input("Random team\nManual team\n")
 
-        if userinput == "M" or "m":
-            PokeTeam.choose_manually
-        elif userinput == "R" or "r":
-            PokeTeam.choose_randomly
+        if userinput == "Manual":
+            self.TrainerTeam.choose_manually()
+        elif userinput == "Random":
+            self.TrainerTeam.choose_randomly()
         else:
             raise Exception("Not an option")     
             
 
     def get_team(self) -> PokeTeam:
-        raise self.__str__
+        return self.TrainerTeam.team
 
     def get_name(self) -> str:
-        raise self.name
+        return self.TrainerName
 
     def register_pokemon(self, pokemon: Pokemon) -> None:
         
-        for i in range(PokeTeam.pokedex.__len__):
-            if PokeTeam.pokedex[i]!=pokemon.get_poketype:
-                PokeTeam.pokedex.__setitem__(pokemon.get_poketype)
-        
-
-    
+        for i in range(self.pokedex.__len__()):
+            if self.pokedex.__contains__(pokemon.get_poketype())==False:
+                self.pokedex.add(pokemon.get_poketype())
 
     def get_pokedex_completion(self) -> float:
-        counter =0
-        for i in range(PokeTeam.pokedex.__len__):
-            if PokeTeam.pokedex[i] in PokeType:
-                counter+=1
-
+        counter=0
+        for i in range(self.pokedex.__len__()):
+            counter+=1
         return round(counter/15,2)
 
     def __str__(self) -> str:       
-        return "Trainer <"+Trainer.get_name+"> Pokedex Completion: <"+Trainer.get_pokedex_completion*100+">%"
+        return "Trainer <"+self.get_name()+"> Pokedex Completion: <"+str(self.get_pokedex_completion()*100)+">%"
 
 if __name__ == '__main__':
     t = Trainer('Ash')
     print(t)
-    t.pick_team("Random")
+    t.pick_team("Manual")
     print(t)
     print(t.get_team())
 
